ca62a4ecc2e003ccac6d0c6efe3faa2f224fdc58
nyendwa <nyendwa@yahoo.com>
2024-03-31 16:04:26 +0200

    Cleaned up code

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -6,9 +6,9 @@ $dbusername = 'root';
 $dbpassword = '';
 //host should have the same variable as the localhost server, assuming the default localhost has been changed to 3307, point it out as shown above. If localhost is a default value=3306 then $host=localhost
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
+    $pdo = new PDO("mysql:host=$host; dbname=$dbname", $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Connected to database successfully!";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
