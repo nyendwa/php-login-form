bc5b55341f61a6bbd59f5af381afe8892827d265
nyendwa <nyendwa@yahoo.com>
2024-03-31 04:14:33 +0200

    Resolved a connection glich that was resulted by a wrong localhost port 3307

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,7 +4,7 @@ $host = 'localhost:3307';
 $dbname = 'mydb';
 $dbusername = 'root';
 $dbpassword = '';
-//Connection failed: SQLSTATE[HY000] [2002] No connection could be made because the target machine actively refused it
+//host should have the same variable as the localhost server, assuming the default localhost has been changed to 3307, point it out as shown above. If localhost is a default value=3306 then $host=localhost
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
